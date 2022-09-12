4878de003095510120db150e18d16da475c84a2c
temaweb10 <pashkovjob@gmail.com>
2022-09-12 21:19:30 +0300

    12

--- routes/userRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllUsers } = require("../controllers/userСontroller");
-
-router.get("/api/users", getAllUsers);
-
-module.exports = router;
