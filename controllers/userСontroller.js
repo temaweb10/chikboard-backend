6eb3356e268856feb46c42aeb0e45280cef573bf
temaweb10 <pashkovjob@gmail.com>
2023-01-06 02:57:51 +0300

    1

--- controllers/userСontroller.js
+++ controllers/userСontroller.js
@@ -134,7 +134,7 @@ const removeSubscribe = (req, res) => {
     )
     .then((result) => {
       console.log(result)
-      res.status(200).json({message:'отписан'})
+     /*  res.status(200).json({message:'отписан'}) */
     })
     .catch((err)=>{
       console.log(err);
@@ -147,12 +147,14 @@ const removeSubscribe = (req, res) => {
     )
     .then((result) => {
       console.log(result)
-      res.status(200).json({message:'отписан'})
+      /* res.status(200).json({message:'отписан'}) */
     })
     .catch((err)=>{
       console.log(err);
       res.status(500).json({ message: "Ошибка сервера" });
     })
+
+    res.status(200).json({message:'отписан'})
 };
 
 const deleteUserById = (req, res) => {
